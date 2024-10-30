8a87f37d584619ea26e63974974a8c8c2baa32c5
Ben Brooks <ben@code.org>
2024-10-30 11:28:17 -0700

    Readd accidentally deleted tests

--- dashboard/test/controllers/projects_controller_test.rb
+++ dashboard/test/controllers/projects_controller_test.rb
@@ -541,4 +541,49 @@ class ProjectsControllerTest < ActionController::TestCase
     assert_response :redirect
     assert_redirected_to "/projects/music/#{channel_id}/view"
   end
+
+  test 'submission status returns appropriate status' do
+    channel_id = '123456'
+    @controller.stubs(:storage_decrypt_channel_id).returns([123, 456])
+    test_project = {}
+    Project.stubs(:find_by).returns(test_project)
+    SharedConstants::PROJECT_SUBMISSION_STATUS.each_value do |status|
+      test_project.stubs(:submission_status).returns(status)
+      get :submission_status, params: {project_type: 'music', channel_id: channel_id}
+      assert_response :success
+      response_status = JSON.parse(@response.body)["status"]
+      assert_equal response_status, status
+    end
+  end
+
+  test 'submit project returns bad_request if no submission description' do
+    channel_id = '123456'
+    submission_description = ''
+    post :submit, params: {project_type: 'music', channel_id: channel_id, submissionDescription: submission_description}
+    assert_response :bad_request
+  end
+
+  test 'submit project returns success if all criteria are met' do
+    channel_id = '123456'
+    @controller.stubs(:storage_decrypt_channel_id).returns([123, 456])
+    submission_description = 'this project rocks'
+    test_project = create :project
+    test_project.stubs(:submission_status).returns(SharedConstants::PROJECT_SUBMISSION_STATUS[:CAN_SUBMIT])
+    Project.stubs(:find_by).returns(test_project)
+    Projects.any_instance.stubs(:publish).returns({})
+    post :submit, params: {project_type: 'music', channel_id: channel_id, submissionDescription: submission_description}
+    assert_response :success
+  end
+
+  test 'submit project returns forbidden if criteria not met' do
+    channel_id = '123456'
+    @controller.stubs(:storage_decrypt_channel_id).returns([123, 456])
+    submission_description = 'this project rocks'
+    test_project = create :project
+    test_project.stubs(:submission_status).returns(SharedConstants::PROJECT_SUBMISSION_STATUS[:ALREADY_SUBMITTED])
+    Project.stubs(:find_by).returns(test_project)
+    Projects.any_instance.stubs(:publish).returns({})
+    post :submit, params: {project_type: 'music', channel_id: channel_id, submissionDescription: submission_description}
+    assert_response :forbidden
+  end
 end
