ed390d3aa3f721511d167ae8e82d9f25b90662c7
Seth Nickell <snickell@gmail.com>
2024-10-31 01:07:36 -1000

    sauce_connect.rb: disable --tunnel-domains and see if that helps

--- lib/cdo/sauce_connect.rb
+++ lib/cdo/sauce_connect.rb
@@ -26,14 +26,14 @@ module Cdo
         raise "saucelabs_tunnel_name must be set in locals.yml, 'cdo-tunnel' is a good default choice" unless CDO.saucelabs_tunnel_name
 
         # Regexes defining the localhost domains we want to tunnel:
-        tunnel_domains = [
-          %q(.*\.localhost),
-        ].join(',')
+        # tunnel_domains = [
+        #   %q(.*\.localhost),
+        # ].join(',')
 
         cmd = [
           "sc", "run",
           "--region", "us-west-1",
-          "--tunnel-domains", tunnel_domains,
+          # "--tunnel-domains", tunnel_domains,
           "--tunnel-name", CDO.saucelabs_tunnel_name,
         ]
 
