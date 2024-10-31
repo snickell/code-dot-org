b0194314ba539cf964b0be7cece5ca401f1b7dab
David Bailey <davidsbailey@users.noreply.github.com>
2024-10-31 13:23:05 -0700

    extract computeBubbleStatus into utils file

--- apps/src/templates/rubrics/teacherRubricRedux.ts
+++ apps/src/templates/rubrics/teacherRubricRedux.ts
@@ -6,9 +6,8 @@ import {
 } from '@reduxjs/toolkit';
 
 import {LevelWithProgress} from '@cdo/apps/code-studio/teacherPanelTypes';
+import {computeBubbleStatus} from '@cdo/apps/templates/rubrics/rubricUtils';
 import {RootState} from '@cdo/apps/types/redux';
-import {LevelStatus} from '@cdo/generated-scripts/sharedConstants';
-
 interface TeacherEvaluation {
   feedback: string;
   id: number;
@@ -152,43 +151,6 @@ export const loadAiEvalStatusForAll = createAsyncThunk(
     });
   }
 );
-
-const computeLevelStatus = (level: LevelWithProgress | undefined) => {
-  if (!level || level.status === LevelStatus.not_tried) {
-    return 'NOT_STARTED';
-  } else if (
-    level.status === LevelStatus.attempted ||
-    level.status === LevelStatus.passed
-  ) {
-    return 'IN_PROGRESS';
-  } else if (
-    level.status === LevelStatus.submitted ||
-    level.status === LevelStatus.perfect ||
-    level.status === LevelStatus.completed_assessment ||
-    level.status === LevelStatus.free_play_complete
-  ) {
-    return 'SUBMITTED';
-  } else {
-    return null;
-  }
-};
-
-function computeBubbleStatus(
-  level: LevelWithProgress | undefined,
-  aiEvalStatus: string,
-  hasTeacherFeedback: boolean
-) {
-  if (hasTeacherFeedback) {
-    return 'EVALUATED';
-  }
-  if (aiEvalStatus === 'READY_TO_REVIEW') {
-    return aiEvalStatus;
-  }
-  if (computeLevelStatus(level) === 'SUBMITTED') {
-    return 'SUBMITTED';
-  }
-  return aiEvalStatus;
-}
 
 export const selectStudentProgressStatusMap = createSelector(
   [
