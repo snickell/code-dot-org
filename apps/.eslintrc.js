2781197196c38134a58c1d5eb7a5e3d595c561aa
David Bailey <davidsbailey@users.noreply.github.com>
2024-11-01 23:12:22 -0700

    set reportUnusedDisableDirectives

--- apps/.eslintrc.js
+++ apps/.eslintrc.js
@@ -100,6 +100,7 @@ module.exports = {
     Turtle: 'readonly',
     YT: 'readonly',
   },
+  reportUnusedDisableDirectives: true,
   rules: {
     ...rulesToEventuallyReenable,
     'babel/semi': 'error', // autofixable
