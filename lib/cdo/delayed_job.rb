53e6e3245deb5a2f8ed0b9691fb5037bd2ab85cb
Seth Nickell <snickell@gmail.com>
2024-11-05 16:37:13 -1000

    subclass delayed_job

--- lib/cdo/delayed_job.rb
+++ lib/cdo/delayed_job.rb
@@ -1,4 +1,5 @@
 require 'cdo/chat_client'
+require 'delayed/command'
 
 module Cdo
   module DelayedJob
@@ -33,19 +34,19 @@ module Cdo
       pids_still_running.each {|pid| kill('KILL', pid)}
     end
 
-    # runs bin/delayed_job
-    def self.delayed_job(*arguments)
-      Dir.chdir(dashboard_dir) do
-        RakeUtils.system 'bin/delayed_job', *arguments
-      end
-    end
-
     # Stops half the delayed_job workers first, starts replacements, then replaces
     # the remaining 50%. This keeps us from ever having 0 workers running.
     def self.rolling_deploy_workers(n_workers_to_start)
-      ChatClient.log("Doing a rolling deploy of #{n_workers_to_start} delayed_job workers")
-      pid_dir = dashboard_dir('tmp/pids')
+      pid = fork do
+        # pre-cache Rails environment so each `delayed_job` worker command invocation
+        # doesn't take N minutes on production.
+        before_worker_fork
+        _rolling_deploy_workers(n_workers_to_start)
+      end
+      Process.wait(pid)
+    end
 
+    def self._rolling_deploy_workers(n_workers_to_start)
       # The goal here is to do a rolling restart of workers. `delayed_job` does not
       # support this, but we've hacked it in. delayed_job numbers workers 0 through N.
       # First, we'll stop workers 0 through N/2, then start N/2 new workers. Then we'll
@@ -56,6 +57,8 @@ module Cdo
               sort_by {|file| file[/\d+/].to_i}.
               map {|file| File.read(file).to_i}
 
+      ChatClient.log("delayed_job: rolling deploy of #{n_workers_to_start} workers, replacing #{pids.size} existing workers")
+
       # Phase 1: stop the first 50% of existing workers
       n_pids_to_stop = pids.empty? ? 0 : pids.size / 2
       first_half_of_pids = pids.first(n_pids_to_stop)
@@ -63,17 +66,68 @@ module Cdo
 
       # Phase 2: start the same number of new workers
       n_workers_started = n_pids_to_stop
-      delayed_job '-n', n_workers_started, 'start'
+      start_n_workers(n_workers_started, initial_worker_index: 0)
 
       # Phase 3: stop the remaining worker pids
       second_half_of_pids = pids - first_half_of_pids
       stop_workers(second_half_of_pids)
 
       # Phase 4: start remaining new workers
-      delayed_job '-n', n_workers_to_start - n_workers_started, 'start'
       # PROBLEM: this will replace the workers that were just started, not add new ones
+      start_n_workers(n_workers_to_start - n_workers_started, initial_worker_index: n_workers_started)
 
-      ChatClient.log("Done starting #{n_workers_to_start} delayed_job workers")
+      ChatClient.log("delayed_job: rolling deploy done, started #{n_workers_to_start} workers")
+    end
+
+    def self.pid_dir
+      dashboard_dir('tmp/pids')
+    end
+
+    def self.log_dir
+      dashboard_dir('log')
+    end
+
+    # run bin/delayed_job by forking our custom Cdo::DelayedJob::Command subclass
+    def self.start_n_workers(n_workers, initial_worker_index:)
+      ChatClient.log("delayed_job: starting #{n_workers} workers, initial_worker_index=#{initial_worker_index}")
+      pid = fork do
+        Cdo::DelayedJob::Command.new.start_n_workers(n_workers, initial_worker_index: initial_worker_index)
+      end
+      Process.wait(pid) # wait for the workers to start
+    end
+
+    # Load rails environment into memory before forking workers so
+    # that they share all this memory and don't have to reload it.
+    def self.before_worker_fork
+      require dashboard_dir('config', 'environment')
+      Delayed::Worker.before_fork
+    end
+
+    # Subclass Delayed::Command from delayed_job/command and monkeypatch it
+    # to allow us to specify the initial worker index when spawning =
+    # new delayed_job.N workers. This allows us to start workers in multiple
+    # batches.
+    #
+    # By calling before_worker_fork before forking the command processes
+    # our multiple batches even share memory.
+    class Command < Delayed::Command
+      def initialize
+        @options = {
+          :quiet => true,
+          :pid_dir => Cdo::DelayedJob.pid_dir,
+          :log_dir => Cdo::DelayedJob.log_dir,
+        }
+        @args = ["start"]
+      end
+
+      def start_n_workers(n_workers, initial_worker_index: 0)
+        Cdo::DelayedJob.before_worker_fork
+        n_workers.times do |worker_index|
+          process_name = "delayed_job.#{worker_index + initial_worker_index}"
+          puts "\tstarting delayed_job worker: #{process_name}"
+          run_process(process_name, @options)
+        end
+      end
     end
   end
 end
