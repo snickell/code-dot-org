b73efc4d606158f9f519644ce2714d9fc34e3dec
Mike Harvey <mike.harvey@code.org>
2024-11-07 11:45:41 -0500

    inline imports

--- apps/src/sites/studio/pages/blocklyLocaleImports.js
+++ apps/src/sites/studio/pages/blocklyLocaleImports.js
@@ -1,125 +1,69 @@
-import * as ar from 'blockly/msg/ar';
-import * as az from 'blockly/msg/az';
-import * as bg from 'blockly/msg/bg';
-import * as bs from 'blockly/msg/bs';
-import * as ca from 'blockly/msg/ca';
-import * as cs from 'blockly/msg/cs';
-import * as da from 'blockly/msg/da';
-import * as de from 'blockly/msg/de';
-import * as el from 'blockly/msg/el';
-import * as en from 'blockly/msg/en';
-import * as es from 'blockly/msg/es';
-import * as et from 'blockly/msg/et';
-import * as eu from 'blockly/msg/eu';
-import * as fa from 'blockly/msg/fa';
-import * as fi from 'blockly/msg/fi';
-import * as fr from 'blockly/msg/fr';
-import * as gl from 'blockly/msg/gl';
-import * as he from 'blockly/msg/he';
-import * as hi from 'blockly/msg/hi';
-import * as hr from 'blockly/msg/hr';
-import * as hu from 'blockly/msg/hu';
-import * as hy from 'blockly/msg/hy';
-import * as id from 'blockly/msg/id';
-import * as is from 'blockly/msg/is';
-import * as it from 'blockly/msg/it';
-import * as ja from 'blockly/msg/ja';
-import * as ka from 'blockly/msg/ka';
-import * as km from 'blockly/msg/km';
-import * as ko from 'blockly/msg/ko';
-import * as ky from 'blockly/msg/ky';
-import * as lt from 'blockly/msg/lt';
-import * as lv from 'blockly/msg/lv';
-import * as my from 'blockly/msg/my';
-import * as nb from 'blockly/msg/nb';
-import * as nl from 'blockly/msg/nl';
-import * as pl from 'blockly/msg/pl';
-import * as pt from 'blockly/msg/pt';
-import * as ptBr from 'blockly/msg/pt-br';
-import * as ro from 'blockly/msg/ro';
-import * as ru from 'blockly/msg/ru';
-import * as si from 'blockly/msg/si';
-import * as sk from 'blockly/msg/sk';
-import * as sl from 'blockly/msg/sl';
-import * as sq from 'blockly/msg/sq';
-import * as sr from 'blockly/msg/sr';
-import * as sv from 'blockly/msg/sv';
-import * as ta from 'blockly/msg/ta';
-import * as te from 'blockly/msg/te';
-import * as th from 'blockly/msg/th';
-import * as tr from 'blockly/msg/tr';
-import * as uk from 'blockly/msg/uk';
-import * as uz from 'blockly/msg/uz';
-import * as vi from 'blockly/msg/vi';
-import * as zhHans from 'blockly/msg/zh-hans';
-import * as zhHant from 'blockly/msg/zh-hant';
-
 // A map of user locales supported by Code.org to locales provided by Google Blockly.
 // For more information, see: https://github.com/google/blockly/tree/master/msg/json
 export const blocklyLocaleMap = {
-  'ar-SA': ar,
-  'az-AZ': az,
-  'bg-BG': bg,
-  'bs-BA': bs,
-  'ca-ES': ca,
-  'cs-CZ': cs,
-  'da-DK': da,
-  'de-DE': de,
-  'el-GR': el,
-  'en-US': en,
-  'es-ES': es,
-  'es-MX': es,
-  'et-EE': et,
-  'eu-ES': eu,
-  'fa-IR': fa,
-  'fi-FI': fi,
-  'fil-PH': en, // English as alternative for Filipino
-  'fr-FR': fr,
-  'ga-IE': en, // English as alternative for Irish
-  'gl-ES': gl,
-  'he-IL': he,
-  'hi-IN': hi,
-  'hr-HR': hr,
-  'hu-HU': hu,
-  'hy-AM': hy,
-  'id-ID': id,
-  'is-IS': is,
-  'it-IT': it,
-  'ja-JP': ja,
-  'ka-GE': ka,
-  'ko-KR': ko,
-  'kk-KZ': ru, // Russian as alternative for Kazakh
-  'km-KH': km,
-  'ky-KG': ky,
-  'lt-LT': lt,
-  'lv-LV': lv,
-  'mi-NZ': en, // English as alternative for Maori
-  'mn-MN': ru, // Russian as alternative for Mongolian
-  'mr-IN': hi, // Hindi as alternative for Marathi
-  'my-MM': my,
-  'nl-NL': nl,
-  'nn-NO': nb, // Norwegian Bokmål as alternative for Nynorsk
-  'no-NO': nb,
-  'pl-PL': pl,
-  'pt-BR': ptBr,
-  'pt-PT': pt,
-  'ro-RO': ro,
-  'ru-RU': ru,
-  'se-FI': nb, // Norwegian Bokmål as alternative for Northern Sami
-  'si-LK': si,
-  'sk-SK': sk,
-  'sl-SI': sl,
-  'sm-WS': en, // English as alternative for Samoan
-  'sq-AL': sq,
-  'sr-SP': sr,
-  'sv-SE': sv,
-  'ta-IN': ta,
-  'te-IN': te,
-  'th-TH': th,
-  'tr-TR': tr,
-  'uk-UA': uk,
-  'uz-UZ': uz,
-  'vi-VN': vi,
-  'zh-CN': zhHans,
-  'zh-TW': zhHant,
+  'ar-SA': require('blockly/msg/ar'),
+  'az-AZ': require('blockly/msg/az'),
+  'bg-BG': require('blockly/msg/bg'),
+  'bs-BA': require('blockly/msg/bs'),
+  'ca-ES': require('blockly/msg/ca'),
+  'cs-CZ': require('blockly/msg/cs'),
+  'da-DK': require('blockly/msg/da'),
+  'de-DE': require('blockly/msg/de'),
+  'el-GR': require('blockly/msg/el'),
+  'en-US': require('blockly/msg/en'),
+  'es-ES': require('blockly/msg/es'),
+  'es-MX': require('blockly/msg/es'),
+  'et-EE': require('blockly/msg/et'),
+  'eu-ES': require('blockly/msg/eu'),
+  'fa-IR': require('blockly/msg/fa'),
+  'fi-FI': require('blockly/msg/fi'),
+  'fil-PH': require('blockly/msg/en'), // English provided as broader alternative for Filipino
+  'fr-FR': require('blockly/msg/fr'),
+  'ga-IE': require('blockly/msg/en'), // English provided as broader alternative for Irish (Gaelic)
+  'gl-ES': require('blockly/msg/gl'),
+  'he-IL': require('blockly/msg/he'),
+  'hi-IN': require('blockly/msg/hi'),
+  'hr-HR': require('blockly/msg/hr'),
+  'hu-HU': require('blockly/msg/hu'),
+  'hy-AM': require('blockly/msg/hy'),
+  'id-ID': require('blockly/msg/id'),
+  'is-IS': require('blockly/msg/is'),
+  'it-IT': require('blockly/msg/it'),
+  'ja-JP': require('blockly/msg/ja'),
+  'ka-GE': require('blockly/msg/ka'),
+  'ko-KR': require('blockly/msg/ko'),
+  'kk-KZ': require('blockly/msg/ru'), // Russian provided as regional alternative for Kazakh
+  'km-KH': require('blockly/msg/km'),
+  'ky-KG': require('blockly/msg/ky'),
+  'lt-LT': require('blockly/msg/lt'),
+  'lv-LV': require('blockly/msg/lv'),
+  'mi-NZ': require('blockly/msg/en'), // English provided as broader alternative for Maori
+  'mn-MN': require('blockly/msg/ru'), // Russian provided as regional alternative for Mongolian
+  'mr-IN': require('blockly/msg/hi'), // Hindi provided as regional alternative for Marathi
+  'my-MM': require('blockly/msg/my'),
+  'nl-NL': require('blockly/msg/nl'),
+  'nn-NO': require('blockly/msg/nb'), // Norwegian Bokmål provided as alternative written standard for Norwegian Nynorsk
+  'no-NO': require('blockly/msg/nb'),
+  'pl-PL': require('blockly/msg/pl'),
+  'pt-BR': require('blockly/msg/pt-br'),
+  'pt-PT': require('blockly/msg/pt'),
+  'ro-RO': require('blockly/msg/ro'),
+  'ru-RU': require('blockly/msg/ru'),
+  'se-FI': require('blockly/msg/nb'), // Norwegian Bokmål provided as regional alternative for Northern Sami
+  'si-LK': require('blockly/msg/si'),
+  'sk-SK': require('blockly/msg/sk'),
+  'sl-SI': require('blockly/msg/sl'),
+  'sm-WS': require('blockly/msg/en'), // English provided as broader alternative for Samoan
+  'sq-AL': require('blockly/msg/sq'),
+  'sr-SP': require('blockly/msg/sr'),
+  'sv-SE': require('blockly/msg/sv'),
+  'ta-IN': require('blockly/msg/ta'),
+  'te-IN': require('blockly/msg/te'),
+  'th-TH': require('blockly/msg/th'),
+  'tr-TR': require('blockly/msg/tr'),
+  'uk-UA': require('blockly/msg/uk'),
+  'uz-UZ': require('blockly/msg/uz'),
+  'vi-VN': require('blockly/msg/vi'),
+  'zh-CN': require('blockly/msg/zh-hans'),
+  'zh-TW': require('blockly/msg/zh-hant'),
 };
