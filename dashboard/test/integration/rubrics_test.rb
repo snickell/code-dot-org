91c94a383c9c116b1aae2533a42703e08bcaaffb
David Bailey <davidsbailey@users.noreply.github.com>
2024-11-12 15:16:04 -0800

    integration test - can still show alert for other lessons

--- dashboard/test/integration/rubrics_test.rb
+++ dashboard/test/integration/rubrics_test.rb
@@ -7,12 +7,12 @@ class ScriptsTest < ActionDispatch::IntegrationTest
     teacher = create :authorized_teacher
     sign_in teacher
 
-    unit = create :script, :with_levels, name: 'test-unit', is_course: true, family_name: 'csd', version_year: '2024'
+    unit = create :script, :with_levels, lessons_count: 2, name: 'test-unit', is_course: true, family_name: 'csd', version_year: '2024'
     CourseOffering.add_course_offering(unit)
     script_level = unit.script_levels.first
+    script_level_path = build_script_level_path(script_level)
     create :rubric, lesson: script_level.lesson, level: script_level.levels.first
 
-    script_level_path = build_script_level_path(script_level)
     get script_level_path
     assert_response :success
     rubric_data = JSON.parse(css_select('script[data-rubricdata]').first.attribute('data-rubricdata').to_s)
@@ -21,9 +21,19 @@ class ScriptsTest < ActionDispatch::IntegrationTest
     post '/api/v1/users/set_seen_ta_scores', params: {lesson_position: script_level.lesson.absolute_position}
     assert_response :success
 
+    # can no longer show alert for this lesson
     get script_level_path
     assert_response :success
     rubric_data = JSON.parse(css_select('script[data-rubricdata]').first.attribute('data-rubricdata').to_s)
     refute rubric_data['canShowTaScoresAlert']
+
+    # can still show alert for other lessons
+    other_script_level = unit.script_levels.last
+    refute_equal other_script_level.lesson.absolute_position, script_level.lesson.absolute_position
+    create :rubric, lesson: other_script_level.lesson, level: other_script_level.levels.first
+    get build_script_level_path(other_script_level)
+    assert_response :success
+    rubric_data = JSON.parse(css_select('script[data-rubricdata]').first.attribute('data-rubricdata').to_s)
+    assert rubric_data['canShowTaScoresAlert']
   end
 end
