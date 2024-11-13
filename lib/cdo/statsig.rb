e4ed727544a2b32fc65e8e110531b8cba64f92c2
wilkie <wilkie05@gmail.com>
2024-11-13 14:22:33 -0500

    Turns on Ruby/JS metrics.

--- lib/cdo/statsig.rb
+++ lib/cdo/statsig.rb
@@ -10,7 +10,7 @@ module Cdo
       # environments. Anything else that runs on the Test server, as well as
       # code that executes in the continous integration builds, will run in local_mode.
       # This limits the number of metrics we emit, thereby lowering our credit usage.
-      local_mode = CDO.rack_env?(:production) || managed_test_environment ? false : true
+      local_mode = false
       options = StatsigOptions.new({'tier' => CDO.rack_env}, network_timeout: 10, logging_interval_seconds: 30, local_mode: local_mode)
       # Initialize Statsig
       Statsig.initialize(CDO.statsig_server_secret_key, options)
