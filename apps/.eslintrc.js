136ee9b76020d62e860dadfef73cc2be24e444f0
David Bailey <davidsbailey@users.noreply.github.com>
2024-11-13 08:59:58 -0800

    remove unused no-restricted-imports rule from global eslintrc

--- apps/.eslintrc.js
+++ apps/.eslintrc.js
@@ -165,16 +165,19 @@ module.exports = {
             message: 'Use jest matchers instead of chai',
           },
           {
+            // We are deprecating enzyme in favor of react-testing-library. See further work at https://docs.google.com/document/d/1eX-LV7d2GtuAypy9BYiT5HOwu7FQjc4kVu6qx_6bDiI
             name: 'enzyme',
             message: 'Use react-testing-library instead of enzyme',
           },
           {
+            // In Feb 2022, we voted to deprecate Radium (proposal at https://docs.google.com/document/d/1Y3uK_iYMhTUaCI6yIDwOAMprIJCuzXSs2fECQggwp60/edit#heading=h.htf3pg55q2kt)
             name: 'radium',
             message: 'Use css modules instead of radium',
           },
           {
+            // We are now using 'react-bootstrap-2'. See further work at https://github.com/code-dot-org/code-dot-org/pull/51681
             name: 'react-bootstrap',
-            message: 'Use design system components instead of react-bootstrap',
+            message: 'Use react-bootstrap-2 instead of react-bootstrap',
           },
           {
             name: 'react-router',
@@ -182,7 +185,7 @@ module.exports = {
           },
           {
             name: 'sinon',
-            message: 'Use jest spys and stubs instead of sinon',
+            message: 'Use jest spies and stubs instead of sinon',
           },
           {
             name: 'sinon-chai',
