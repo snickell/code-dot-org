05b9145ea16ebebb62a70b3185e8b66591a3cdd6
Stephen Liang <538214+stephenliang@users.noreply.github.com>
2024-11-20 21:52:05 +0000

    Revert "chore: remove dsco components for PR"
    This reverts commit 0754ce5d94fb6eddc7c9af2a3e70d3a8677f6e79.

---
+++ frontend/packages/dsco/src/breadcrumbs/index.ts
@@ -0,0 +1,3 @@
+export {default as Breadcrumbs} from './Breadcrumbs';
+export type {BreadcrumbsProps} from './Breadcrumbs';
+export {default as default} from './Breadcrumbs';
---
+++ frontend/packages/dsco/src/common/hooks/index.ts
@@ -0,0 +1,18 @@
+import useBodyScrollLock from '@/common/hooks/useBodyScrollLock';
+import useDocumentKeydown from '@/common/hooks/useDocumentKeydown';
+import useEscapeKeyHandler from '@/common/hooks/useEscapeKeyHandler';
+import useFocusTrap from '@/common/hooks/useFocusTrap';
+
+export {
+  useBodyScrollLock,
+  useDocumentKeydown,
+  useEscapeKeyHandler,
+  useFocusTrap,
+};
+
+export default {
+  useBodyScrollLock,
+  useDocumentKeydown,
+  useEscapeKeyHandler,
+  useFocusTrap,
+};
