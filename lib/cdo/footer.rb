a79ed9988406b2579adb697389071e032f4f234b
wilkie <wilkie05@gmail.com>
2024-11-21 01:25:57 -0500

    Fixes footer locale options on dashboard.

--- lib/cdo/footer.rb
+++ lib/cdo/footer.rb
@@ -4,7 +4,7 @@ module Cdo
   class Footer
     def self.get_locale_options(options)
       ge_region = options[:ge_region] || :root
-      @locale_options = Cdo::GlobalEdition.locales_for(ge_region)
+      @locale_options = Cdo::GlobalEdition.region_locales(ge_region)
     end
 
     def self.get_footer_contents(options)
