4e6ae252240835275a044930219c84686e5aac57
Sanchit Malhotra <sanchit@code.org>
2024-11-22 14:28:07 -0800

    Working end to end + update tests

--- lib/cdo/aws/cloudwatch_logs.rb
+++ lib/cdo/aws/cloudwatch_logs.rb
@@ -10,7 +10,7 @@ module Cdo
       attr_accessor :client
     end
 
-    BUFFERS = Hash.new {|h, key| h[key] = Buffer.new(key)}
+    BUFFERS = Hash.new {|h, key| h[key] = Buffer.new(key.split("/").first, key.split("/").last)}
 
     # Ref: http://docs.aws.amazon.com/AmazonCloudWatchLogs/latest/APIReference/API_PutLogEvents.html
     MAXIUMUM_BATCH_SIZE = 1024 * 1024
@@ -39,13 +39,15 @@ module Cdo
           http_read_timeout: 5,
           http_idle_timeout: 2
         )
-        client.put_log_events(
+        resp = client.put_log_events(
           log_group_name: @log_group_name,
           log_stream_name: @log_stream_name,
           log_events: events
         )
+        Honeybadger.notify("Log events rejected #{resp.rejected_log_events_info.to_json}") if resp.rejected_log_events_info
       rescue => exception
         Honeybadger.notify(exception)
+        puts "Error sending logs to group/stream #{@log_group_name}/#{@log_stream_name}: #{exception.full_message}" if rack_env?(:development)
       end
 
       def size(events)
@@ -54,9 +56,26 @@ module Cdo
       end
     end
 
+    # @param [String] log_group_name
+    # @param [String] log_stream_name
+    # @param [Hash, Aws::CloudWatchLogs::Types::InputLogEvent] log event
+    def self.put_log_event(log_group_name, log_stream_name, event)
+      raise "Event message is too large" if event[:message].bytesize > MAXIMUM_EVENT_SIZE
+      BUFFERS["#{log_group_name}/#{log_stream_name}"].buffer(event)
+    end
+
+    # Asynchronously send a collection of CloudWatch logs in batches.
+    # @param [String] log_group_name
+    # @param [String] log_stream_name
+    # @param metrics [Array<Aws::CloudWatch::Types::MetricDatum>]
     def self.put_log_events(log_group_name, log_stream_name, events)
-      raise "Event message is too large" if events.any? {|event| event[:message].bytesize > MAXIMUM_EVENT_SIZE}
-      BUFFERS["#{log_group_name}/#{log_stream_name}"].push(events)
+      events.each do |event|
+        put_log_event(log_group_name, log_stream_name, event)
+      end
+    end
+
+    def self.flush!
+      BUFFERS.each_value(&:flush!)
     end
   end
 end
--- lib/test/cdo/aws/test_cloudwatch_logs.rb
+++ lib/test/cdo/aws/test_cloudwatch_logs.rb
@@ -7,15 +7,32 @@ class CdoCloudWatchLogsTest < Minitest::Test
   end
 
   def test_put_log_events
-    Cdo::CloudWatchLogs.put_log_events('log_group_name', 'log_stream_name', [{message: 'message'}])
+    events = [
+      {message: 'message1', timestamp: (Time.now.to_f * 1000).to_i},
+      {message: 'message2', timestamp: (Time.now.to_f * 1000).to_i},
+      {message: 'message3', timestamp: (Time.now.to_f * 1000).to_i}
+    ]
+    Cdo::CloudWatchLogs.put_log_event('log_group_name', 'log_stream_name', events[0])
+    Cdo::CloudWatchLogs.put_log_events('log_group_name', 'log_stream_name', [events[1], events[2]])
     Cdo::CloudWatchLogs.flush!
     refute_empty Cdo::CloudWatchLogs.client.api_requests
     assert_equal(
       {
         log_group_name: 'log_group_name',
         log_stream_name: 'log_stream_name',
-        log_events: [{message: 'message'}]
+        log_events: events
       }, Cdo::CloudWatchLogs.client.api_requests.first[:params]
     )
+  end
+
+  def test_rejected_log_events
+    Honeybadger.expects(:notify).once
+    Cdo::CloudWatchLogs.client.stub_data(:put_log_events, rejected_log_events_info: {expired_log_event_end_index: 1})
+    Cdo::CloudWatchLogs.put_log_event(
+      'log_group_name',
+      'log_stream_name',
+      {message: 'message', timestamp: (Time.now.to_f * 1000).to_i}
+    )
+    Cdo::CloudWatchLogs.flush!
   end
 end
