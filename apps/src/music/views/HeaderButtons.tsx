5c691d1a3a009f88bcc72135e1ecb993a8f05282
dju90 <bryan@code.org>
2024-11-27 14:17:02 -0800

    fix linter errors

--- apps/src/music/views/HeaderButtons.tsx
+++ apps/src/music/views/HeaderButtons.tsx
@@ -22,8 +22,10 @@ interface CurrentPackProps {
   noRightPadding: boolean;
 }
 
-const TEACHER_FEEDBACK_LINK = 'https://docs.google.com/forms/d/e/1FAIpQLSflGeMmY_ff1QllJfpTsWGZdn_xv6dKpPba_evTMwfbvG3FTA/viewform';
-const STUDENT_FEEDBACK_LINK = 'https://docs.google.com/forms/d/e/1FAIpQLSeZGNgX4wDvA29stId_Q2toofJN-r12zSP8yBMZ-E9KW5XPWg/viewform';
+const TEACHER_FEEDBACK_LINK =
+  'https://docs.google.com/forms/d/e/1FAIpQLSflGeMmY_ff1QllJfpTsWGZdn_xv6dKpPba_evTMwfbvG3FTA/viewform';
+const STUDENT_FEEDBACK_LINK =
+  'https://docs.google.com/forms/d/e/1FAIpQLSeZGNgX4wDvA29stId_Q2toofJN-r12zSP8yBMZ-E9KW5XPWg/viewform';
 
 const CurrentPack: React.FunctionComponent<CurrentPackProps> = ({
   packFolder,
