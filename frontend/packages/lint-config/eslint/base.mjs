c54622d75cdd8581d04d232a5fad6cb59dc4c62e
Stephen Liang <538214+stephenliang@users.noreply.github.com>
2024-12-02 23:38:43 +0000

    chore(eslint): move ts-eslint to base config
    Previously, the ts-eslint recommended linter was only added to the react config. However, it makes more sense to have it in the base config as Code.org node libraries will use Typescript.
    
    We will re-review this when the standards working group meets.

--- frontend/packages/lint-config/eslint/base.mjs
+++ frontend/packages/lint-config/eslint/base.mjs
@@ -1,8 +1,10 @@
 import importPlugin from 'eslint-plugin-import-x';
+import {configs as tseslintConfig} from 'typescript-eslint';
 import js from '@eslint/js';
 
 export default [
   js.configs.recommended,
+  ...tseslintConfig.recommended,
   importPlugin.flatConfigs.recommended,
   importPlugin.flatConfigs.typescript,
   {
