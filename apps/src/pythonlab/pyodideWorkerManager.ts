ee4a844b89ce96b9c6d6f177706fb312d8a7603c
Molly Moen <molly@code.org>
2024-12-03 14:51:45 -0800

    refactor

--- apps/src/pythonlab/pyodideWorkerManager.ts
+++ apps/src/pythonlab/pyodideWorkerManager.ts
@@ -19,6 +19,7 @@ import {PyodideMessage} from './types';
 let callbacks: {[key: number]: (event: PyodideMessage) => void} = {};
 let inputServiceWorker: ServiceWorker | undefined;
 let lastInputId = -1;
+let setupPromise: Promise<void> | undefined;
 
 const setUpPyodideWorker = () => {
   // @ts-expect-error because TypeScript does not like this syntax.
@@ -81,68 +82,81 @@ const setUpPyodideWorker = () => {
         );
         break;
     }
-
-    if ('serviceWorker' in navigator) {
-      try {
-        const url = new URL(
-          './inputServiceWorker.js',
-          // @ts-expect-error because TypeScript does not like this syntax.
-          import.meta.url
-        );
-        const registration = await navigator.serviceWorker.register(url);
-        if (registration.active) {
-          console.debug('Service worker active');
-          inputServiceWorker = registration.active;
-        }
-
-        registration.addEventListener('updatefound', () => {
-          const installingWorker = registration.installing;
-          if (installingWorker) {
-            console.debug('Installing new service worker');
-            installingWorker.addEventListener('statechange', () => {
-              if (installingWorker.state === 'installed') {
-                console.debug('New service worker installed');
-                inputServiceWorker = installingWorker;
-              }
-            });
-          }
-        });
-      } catch (error) {
-        console.error(`Registration failed with ${error}`);
-      }
-
-      navigator.serviceWorker.onmessage = event => {
-        console.log(`got message in main thread with type ${event.data.type}`);
-        if (event.data.type === 'CDO_PY_AWAITING_INPUT') {
-          console.log('got input request in main thread');
-          if (event.source instanceof ServiceWorker) {
-            // Update the service worker reference, in case the service worker is different to the one we registered
-            inputServiceWorker = event.source;
-          }
-          lastInputId = event.data.id;
-          // TODO: do we need to support waiting for multiple inputs at once?? Do we want to be smarter
-          // about only accepting input when we are awaiting a response?
-        }
-      };
-    } else {
-      console.error('Service workers not supported');
-    }
   };
 
   return worker;
 };
 
+const registerServiceWorker = async () => {
+  if ('serviceWorker' in navigator) {
+    try {
+      const url = new URL(
+        './inputServiceWorker.js',
+        // @ts-expect-error because TypeScript does not like this syntax.
+        import.meta.url
+      );
+      const registration = await navigator.serviceWorker.register(url);
+      if (registration.active) {
+        console.debug('Service worker active');
+        inputServiceWorker = registration.active;
+      }
+
+      registration.addEventListener('updatefound', () => {
+        const installingWorker = registration.installing;
+        if (installingWorker) {
+          console.debug('Installing new service worker');
+          installingWorker.addEventListener('statechange', () => {
+            if (installingWorker.state === 'installed') {
+              console.debug('New service worker installed');
+              inputServiceWorker = installingWorker;
+            }
+          });
+        }
+      });
+    } catch (error) {
+      console.error(`Registration failed with ${error}`);
+    }
+
+    navigator.serviceWorker.onmessage = event => {
+      console.log(`got message in main thread with type ${event.data.type}`);
+      if (event.data.type === 'CDO_PY_AWAITING_INPUT') {
+        console.log('got input request in main thread');
+        if (event.source instanceof ServiceWorker) {
+          // Update the service worker reference, in case the service worker is different to the one we registered
+          inputServiceWorker = event.source;
+        }
+        lastInputId = event.data.id;
+        // TODO: do we need to support waiting for multiple inputs at once?? Do we want to be smarter
+        // about only accepting input when we are awaiting a response?
+      }
+    };
+  } else {
+    console.error('Service workers not supported');
+  }
+};
+
+const initializeServiceWorker = async () => {
+  if (!setupPromise) {
+    setupPromise = registerServiceWorker();
+  }
+  await setupPromise;
+};
+
+initializeServiceWorker();
 let pyodideWorker = setUpPyodideWorker();
 
 const asyncRun = (() => {
   let id = 0; // identify a Promise
-  return (
+  return async (
     script: string,
     source: MultiFileSource,
     validationFile?: ProjectFile
   ) => {
     // the id could be generated more carefully
     id = (id + 1) % Number.MAX_SAFE_INTEGER;
+
+    // Make sure async setup is done
+    await initializeServiceWorker();
     return new Promise<PyodideMessage>(onSuccess => {
       callbacks[id] = onSuccess;
       const messageData = {
