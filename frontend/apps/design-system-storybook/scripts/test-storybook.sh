9940ac1c83b156070cd5b151c4fd674edd06704d
Stephen Liang <538214+stephenliang@users.noreply.github.com>
2024-12-03 23:20:52 +0000

    chore(storybook): clarify test script names
    The `test-storybook` script is now `test:ui:local` to better reflect its purpose for locally testing storybook.
    
    Additionally, the `test:ui` script is now `test` to better reflect its purpose for running tests on the CI environment.

--- frontend/apps/design-system-storybook/scripts/test-storybook.sh
+++ frontend/apps/design-system-storybook/scripts/test-storybook.sh
@@ -10,4 +10,4 @@ yarn build-storybook --quiet
 # Wait for Storybook to be available and then execute the tests
 npx concurrently -k -s first -n "SB,TEST" -c "magenta,blue" \
             "npx http-server storybook-static --port 6006 --silent" \
-            "npx wait-on tcp:127.0.0.1:6006 && yarn test-storybook"
+            "npx wait-on tcp:127.0.0.1:6006 && yarn test:ui:local"
