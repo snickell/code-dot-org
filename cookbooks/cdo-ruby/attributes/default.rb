d8ac6508c685c648dfcc7fc2eee02b4caadf0139
Seth Nickell <snickell@gmail.com>
2025-01-08 09:33:18 -1000

    Bump gem and bundle versions in chef

--- cookbooks/cdo-ruby/attributes/default.rb
+++ cookbooks/cdo-ruby/attributes/default.rb
@@ -1,6 +1,6 @@
 default['cdo-ruby'] = {
   version: '3.4.1',
-  rubygems_version: '3.5.17 ',
-  bundler_version: '2.5.17',
+  rubygems_version: '3.6.2 ',
+  bundler_version: '2.6.2',
   rake_version: '13.2.1'
 }
--- cookbooks/cdo-ruby/test/integration/default/inspec/ruby_spec.rb
+++ cookbooks/cdo-ruby/test/integration/default/inspec/ruby_spec.rb
@@ -2,5 +2,5 @@ require_relative '../../../shared/helper_spec'
 
 file_exist '/usr/local/bin/ruby'
 cmd 'ruby -v', '3.4.1'
-cmd 'gem -v', '3.5.17'
-cmd 'bundler -v', '2.5.17'
+cmd 'gem -v', '3.6.2'
+cmd 'bundler -v', '2.6.2'
