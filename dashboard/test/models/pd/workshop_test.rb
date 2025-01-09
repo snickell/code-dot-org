34819c4428dbcc3256b924fa7610c1e5dbc3f719
Turner Riley <turner.riley@code.org>
2025-01-09 15:41:20 -0500

    send teacher enrollment reminder email to both emails

--- dashboard/test/models/pd/workshop_test.rb
+++ dashboard/test/models/pd/workshop_test.rb
@@ -789,6 +789,20 @@ class Pd::WorkshopTest < ActiveSupport::TestCase
     assert_includes(e.message, 'bad email')
   end
 
+  test 'sends teacher_enrollment_reminder email to both the users email and alternate email if available and for a summer workshop' do
+    mock_mail = stub
+    mock_mail.stubs(:deliver_now)
+    Pd::WorkshopMailer.expects(:teacher_enrollment_reminder).returns(mock_mail).times(2)
+
+    teacher = create :teacher
+    workshop = create :summer_workshop, course: Pd::SharedWorkshopConstants::COURSE_CSD
+    application = create :pd_teacher_application, course: 'csd', application_year: workshop.school_year, user: teacher, status: 'accepted'
+    create :pd_enrollment, application_id: application.id, user: teacher, workshop: workshop
+    Pd::Workshop.expects(:scheduled_start_in_days).returns([workshop])
+
+    Pd::Workshop.send_reminder_for_upcoming_in_days(1)
+  end
+
   test 'errors in organizer reminders in send_reminder_for_upcoming_in_days do not stop batch' do
     mock_mail = stub
     mock_mail.stubs(:deliver_now).returns(nil).then.returns(nil).then.returns(nil).then.returns(nil).then.returns(nil).then.raises(RuntimeError, 'bad email')
