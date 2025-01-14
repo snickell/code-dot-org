962f9c2b657d38dce62aae7e605c2f05203bcd14
Mario Gil Correa <mario.gil-correa@code.org>
2025-01-14 19:57:40 +0100

    move storybook shored props

--- apps/src/templates/census/CensusTeacherBanner.story.js
+++ apps/src/templates/census/CensusTeacherBanner.story.js
@@ -19,26 +19,21 @@ const Template = args => (
       country: 'US',
       zip: '12345',
     }}
+    question="how_many_10_hours"
+    inClass={true}
+    teacherName="BlakeSmith"
+    teacherEmail="BlakeSmith@gmail.com"
     onSubmitSuccess={action('submitSuccess')}
+    schoolYear={2024}
     {...args}
   />
 );
-export const BasicCensus = Template.bind({});
-BasicCensus.args = {
-  question: 'how_many_10_hours',
-  teacherName: 'BlakeSmith',
-  teacherEmail: 'BlakeSmith@gmail.com',
+export const BasicCensusBanner = Template.bind({});
+BasicCensusBanner.args = {
   teaches: false,
-  inClass: true,
-  schoolYear: 2024,
 };
 
-export const BasicTeacherCensus = Template.bind({});
-BasicTeacherCensus.args = {
-  question: 'how_many_10_hours',
+export const BasicTeacherCensusBanner = Template.bind({});
+BasicTeacherCensusBanner.args = {
   teaches: true,
-  inClass: true,
-  teacherName: 'BlakeSmith',
-  teacherEmail: 'BlakeSmith@gmail.com',
-  schoolYear: 2024,
 };
