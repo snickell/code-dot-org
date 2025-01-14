eb40e9b9d75977e583fc9360e8f845535631fb02
David Bailey <davidsbailey@users.noreply.github.com>
2025-01-14 12:26:39 -0800

    securely hash user id

--- bin/curriculum/export_unit_progress.rb
+++ bin/curriculum/export_unit_progress.rb
@@ -48,6 +48,21 @@ def get_project_source(user_id, level_id, script_id)
   JSON.parse(main_json)['source']
 end
 
+def hashed_user_id(user_id)
+  secret_key = CDO.properties_encryption_key
+  raise "missing CDO.properties_encryption_key" unless secret_key
+
+  # a one-way hash that cannot be reverse-engineered by guessing and
+  # checking user ids without knowing the secret.
+  input_data = "#{user_id}#{secret_key}"
+  digest = Digest::SHA256.hexdigest(input_data)
+
+  # truncate to 128 bits to make digest length more manageable. user ids are
+  # currently 27 bits in 2025. chance of collision is 2^-75 which low enough to ignore.
+  # https://en.wikipedia.org/wiki/Birthday_attack#Simple_approximation
+  digest[0..31]
+end
+
 def main
   filename = File.expand_path('csd3_including_contained_levels_for_stanford.sql', __dir__)
   query = File.read(filename)
@@ -56,6 +71,10 @@ def main
   results.each do |row|
     source = get_project_source(row['user_id'], row['level_id'], row['script_id'])
     row[:source] = source
+
+    row[:hashed_user_id] = hashed_user_id(row['user_id'])
+    row.delete('user_id')
+
     puts JSON.pretty_generate row
   end
 end
