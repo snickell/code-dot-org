bfaa59cb49bc45dd91d217adccfb52367c9d37c4
Stephen Liang <538214+stephenliang@users.noreply.github.com>
2025-01-17 14:44:44 -0800

    update to fix cfn errors (six tries!)

--- aws/cloudformation/standalone/marketing/deployment/config.rb
+++ aws/cloudformation/standalone/marketing/deployment/config.rb
@@ -14,69 +14,7 @@ module Config
   #   autoscaling_target_value (production only): Number of requests / minute at which autoscaling starts to take effect
   #     not used on test
   AVAILABILITY_ZONES = [
-    'us-west-2a',
-    'us-west-2b'
-  ]
-  ENDPOINT_CONFIGS = [
-    {
-      hf_model_id: "mistralai/Mistral-7B-Instruct-v0.1",
-      model_id: SharedConstants::AI_CHAT_MODEL_IDS[:MISTRAL],
-      model_name: "Mistral",
-      instance_type: {
-        production: "ml.g5.4xlarge",
-        test: "ml.g5.xlarge"
-      },
-      min_num_instances: 3,
-      max_num_instances: 4,
-      autoscaling_target_value: 150
-    },
-    {
-      hf_model_id: "BioMistral/BioMistral-7B",
-      model_id: SharedConstants::AI_CHAT_MODEL_IDS[:BIOMISTRAL],
-      model_name: "BioMistral",
-      instance_type: {
-        production: "ml.g5.2xlarge",
-        test: "ml.g5.2xlarge"
-      },
-      min_num_instances: 2,
-      max_num_instances: 2,
-      autoscaling_target_value: 150
-    },
-    {
-      hf_model_id: "upaya07/Arithmo2-Mistral-7B",
-      model_id: SharedConstants::AI_CHAT_MODEL_IDS[:ARITHMO],
-      model_name: "Arithmo",
-      instance_type: {
-        production: "ml.g5.xlarge",
-        test: "ml.g5.xlarge"
-      },
-      min_num_instances: 2,
-      max_num_instances: 2,
-      autoscaling_target_value: 150
-    },
-    {
-      hf_model_id: "phanerozoic/Mistral-Pirate-7b-v0.3",
-      model_id: SharedConstants::AI_CHAT_MODEL_IDS[:PIRATE],
-      model_name: "Pirate",
-      instance_type: {
-        production: "ml.g5.xlarge",
-        test: "ml.g5.xlarge"
-      },
-      min_num_instances: 2,
-      max_num_instances: 2,
-      autoscaling_target_value: 150
-    },
-    {
-      hf_model_id: "FPHam/Karen_TheEditor_V2_CREATIVE_Mistral_7B",
-      model_id: SharedConstants::AI_CHAT_MODEL_IDS[:KAREN],
-      model_name: "Karen",
-      instance_type: {
-        production: "ml.g5.xlarge",
-        test: "ml.g5.xlarge"
-      },
-      min_num_instances: 2,
-      max_num_instances: 2,
-      autoscaling_target_value: 150
-    }
+    'us-east-2a',
+    'us-east-2b'
   ].freeze
 end
--- aws/cloudformation/standalone/marketing/deployment/deploy.rb
+++ aws/cloudformation/standalone/marketing/deployment/deploy.rb
@@ -4,4 +4,4 @@ require_relative './config'
 puts "\nTransforming ERB to YAML..."
   ` erb -T - -r ./config.rb marketing.yml.erb > marketing.yml `
   return unless $?&.exitstatus == 0
-  puts "Success!".colorize(:light_green)
+  puts "Success!"
