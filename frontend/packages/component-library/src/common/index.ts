f95a36251d896dd4135c87f99a723b9da3389b32
denyslevada <levada.denys@gmail.com>
2025-01-17 19:29:52 +0200

    updated exports of dsco package.json

--- frontend/packages/component-library/src/common/index.ts
+++ frontend/packages/component-library/src/common/index.ts
@@ -1,3 +1,3 @@
-export {DropdownColor, DropdownFormFieldRelatedProps, ComponentSizeXSToL, ComponentPlacementDirection} from './types'
+export * as types from './types'
 export * as constants from './constants'
 export * as helpers from './helpers'
