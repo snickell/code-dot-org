1b466d9462fb6aa81bd6535a151e87ba22806307
Erin Bond <erin.bond@code.org>
2025-01-24 12:54:03 -0500

    adjust permissions helpers in user.rb

--- dashboard/config/routes.rb
+++ dashboard/config/routes.rb
@@ -83,7 +83,7 @@ Dashboard::Application.routes.draw do
     resources :images, only: [:new]
 
     get "/ai_iteration/tools", to: "ai_iteration#tools"
-    get "/student_code_samples/:num_samples/:script_id/:level_id", to: "student_code_samples#fetch_student_code_samples"
+    get "/student_code_sample/:num_samples/:script_id/:level_id", to: "student_code_sample#fetch_student_code_samples"
 
     get 'maker/home', to: 'maker#home'
     get 'maker/setup', to: 'maker#setup'
