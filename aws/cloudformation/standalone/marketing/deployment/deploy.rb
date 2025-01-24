cb52b8374afe00c55dd5f38a5ed045800aac221b
Stephen Liang <538214+stephenliang@users.noreply.github.com>
2025-01-24 12:28:04 -0800

    rubocop

--- aws/cloudformation/standalone/marketing/deployment/deploy.rb
+++ aws/cloudformation/standalone/marketing/deployment/deploy.rb
@@ -2,6 +2,6 @@
 require_relative './config'
 
 puts "\nTransforming ERB to YAML..."
-  ` erb -T - -r ./config.rb marketing.yml.erb > marketing.yml `
-  return unless $?&.exitstatus == 0
-  puts "Success!"
+` erb -T - -r ./config.rb marketing.yml.erb > marketing.yml `
+return unless $?&.exitstatus == 0
+puts "Success!"
