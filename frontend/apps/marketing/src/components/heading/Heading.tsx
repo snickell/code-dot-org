81d15bb1f1ba6117555bae793b83cc19dc1c1d1a
denyslevada <levada.denys@gmail.com>
2025-01-24 15:19:45 +0200

    prettier fix

--- frontend/apps/marketing/src/components/heading/Heading.tsx
+++ frontend/apps/marketing/src/components/heading/Heading.tsx
@@ -1,28 +1,61 @@
 import '@code-dot-org/component-library/typography/index.css';
-import {default as Typography, VisualAppearance, SemanticTag} from '@code-dot-org/component-library/typography';
-import React, {ReactNode} from 'react'
+import {
+  default as Typography,
+  VisualAppearance,
+  SemanticTag,
+} from '@code-dot-org/component-library/typography';
+import React, {ReactNode} from 'react';
 
-type HeadingVisualAppearance = Exclude<VisualAppearance, 'body-one' | 'body-two' | 'body-three' | 'body-four' | 'overline-one' | 'overline-two' | 'overline-three' | 'strong' | 'extra-strong' | 'em' | 'figcaption'>
-type HeadingSemanticTag = Exclude<SemanticTag, 'p' | 'strong' | 'em' | 'figcaption' | 'span'>;
+type HeadingVisualAppearance = Exclude<
+  VisualAppearance,
+  | 'body-one'
+  | 'body-two'
+  | 'body-three'
+  | 'body-four'
+  | 'overline-one'
+  | 'overline-two'
+  | 'overline-three'
+  | 'strong'
+  | 'extra-strong'
+  | 'em'
+  | 'figcaption'
+>;
+type HeadingSemanticTag = Exclude<
+  SemanticTag,
+  'p' | 'strong' | 'em' | 'figcaption' | 'span'
+>;
 
 type HeadingProps = {
-    /** Heading content */
-    children: ReactNode;
-    /** Heading visual appearance */
-    visualAppearance: HeadingVisualAppearance;
-}
+  /** Heading content */
+  children: ReactNode;
+  /** Heading visual appearance */
+  visualAppearance: HeadingVisualAppearance;
+};
 
-const headingVisualAppearanceToSemanticTagMap: Record<HeadingVisualAppearance, HeadingSemanticTag> = {
-    'heading-xxl': 'h1',
-    'heading-xl': 'h2',
-    'heading-lg': 'h3',
-    'heading-md': 'h4',
-    'heading-sm': 'h5',
-    'heading-xs': 'h6'
-}
+const headingVisualAppearanceToSemanticTagMap: Record<
+  HeadingVisualAppearance,
+  HeadingSemanticTag
+> = {
+  'heading-xxl': 'h1',
+  'heading-xl': 'h2',
+  'heading-lg': 'h3',
+  'heading-md': 'h4',
+  'heading-sm': 'h5',
+  'heading-xs': 'h6',
+};
 
-const Heading: React.FunctionComponent<HeadingProps> = ({visualAppearance, children}) => {
-    return <Typography semanticTag={headingVisualAppearanceToSemanticTagMap[visualAppearance]} visualAppearance={visualAppearance}>{children}</Typography>
-}
+const Heading: React.FunctionComponent<HeadingProps> = ({
+  visualAppearance,
+  children,
+}) => {
+  return (
+    <Typography
+      semanticTag={headingVisualAppearanceToSemanticTagMap[visualAppearance]}
+      visualAppearance={visualAppearance}
+    >
+      {children}
+    </Typography>
+  );
+};
 
 export default Heading;
